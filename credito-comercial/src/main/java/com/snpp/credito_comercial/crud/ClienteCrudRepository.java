185e9aa71969a6bcd078b6c3be93a3890971a320
MrJrfb <juanrflorentin01@gmail.com>
2026-08-12 15:33:11 -0300

    agregar estructura inicial del proyecto

---
+++ credito-comercial/src/main/java/com/snpp/credito_comercial/crud/ClienteCrudRepository.java
@@ -0,0 +1,8 @@
+package com.snpp.credito_comercial.crud;
+
+import org.springframework.data.repository.CrudRepository;
+import com.snpp.credito_comercial.entity.Cliente;
+
+public interface ClienteCrudRepository extends CrudRepository<Cliente, Long>{
+	
+}
